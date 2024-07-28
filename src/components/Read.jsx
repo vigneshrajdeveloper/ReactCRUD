f5a0fc6628c43b41275768e7ad28fff9ea222b47
Vigneshraj&123 <vigneshdeva31@gmail.com>
2024-07-28 16:15:50 +0530

    Read : Read operation done

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,11 +1,73 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import  axios  from "axios";
+import { API } from "../API";
+import Table from 'react-bootstrap/Table';
+import { Container } from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function Readpage(){
+
+    const[Readusers,SetReadusers]=useState([]);
+
+    const getuserdetail= async ()=>{
+
+      const resp=await axios.get(API+'users');
+      //console.log(resp.data);
+
+      SetReadusers(resp.data);
+
+    }
+
+
+    useEffect(()=>{
+
+      getuserdetail();
+  
+    },[]);
 
     return(
 
         <>
-        <h1>Read Page!!!</h1>
+        
+       <h1>Read Page!!!</h1>
+       <br></br>
+       <Container>
+       <Row>
+        
+        <Col>
+        
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>Feedback</th>
+          <th>status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          Readusers.map((data,indx)=>
+        <tr key={indx}>
+          <td >{data.id}</td>
+          <td>{data.email}</td>
+          <td>{data.feedback}</td>
+          <td>{data.checkbox?'followed':'not followed'}</td>
+        </tr>
+          )
+
+}
+        
+      </tbody>
+    </Table>
+        
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+       
         </>
     ) ;
 }
